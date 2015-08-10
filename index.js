d2a1bea1210256e7f40dcd1664c90730270e28d9
chris <chris.ludden@gmail.com>
2015-08-10 10:16:33 -0600

    initial commit

--- index.js
+++ index.js
@@ -2,5 +2,14 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-identicon'
+    name: 'ember-cli-identicon',
+
+    included: function (app, parentAddon) {
+        var target = (parentAddon || app);
+        this._super.included(target);
+
+        target.import('vendor/sha.js');
+        target.import('vendor/pnglib.js');
+        target.import('vendor/identicon.js');
+    }
 };
